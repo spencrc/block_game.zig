fbd1388deee232d864f3a5d6a13caef7648f9aca
spencrc <145073726+spencrc@users.noreply.github.com>
2025-10-28 00:28:32 -0400

    replaced types.zig with constants.zig

--- src/types.zig
+++
@@ -1,7 +0,0 @@
-const vec3 = @import("math.zig").Vec3;
-
-pub const InstanceData = struct {
-    pos: vec3 = vec3.zero(),
-};
-
-pub const Vertex = extern struct { x: f32, y: f32, z: f32, u: f32, v: f32 };
