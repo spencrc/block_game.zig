92f874c7ed4c3fde0e3bc5444a1110e1653468e3
spencrc <145073726+spencrc@users.noreply.github.com>
2025-10-29 13:48:35 -0400

    clarified chunk variable names; now uses heap allocation to avoid stack overflow

--- src/chunk.zig
+++ src/chunk.zig
@@ -33,69 +33,69 @@ pub fn create() Chunk {
 //TODO: somehow get width/length of final result greedy mesh quads, so we can pass to uniform and then scale texcoords accurately
 //Greedy mesher algorithm implementation in Zig. Works to generate a mesh for a chunk with minimals vertices for performance.
 //Please see here for where the code originates, as it's not my own: https://gist.github.com/Vercidium/a3002bd083cce2bc854c9ff8f0118d33#file-greedyvoxelmeshing-L19
-pub fn greedy_mesh(self: *Chunk) void {
-    var vertices: [6 * 6 * MAX_CUBES_PER_CHUNK]Vertex = undefined;
-    var vertex_count: u16 = 0;
+pub fn greedy_mesh(self: *Chunk, allocator: std.mem.Allocator) void {
+    var vertices_list = std.ArrayList(Vertex).empty;
+    defer vertices_list.deinit(allocator);
 
     for (0..3) |d| {
         const u = (d + 1) % 3;
         const v = (d + 2) % 3;
-        var x: [3]isize = .{ 0, 0, 0 };
+        var pos: [3]isize = .{ 0, 0, 0 };
         var q: [3]isize = .{ 0, 0, 0 };
 
         var mask: [CHUNK_SIZE * CHUNK_SIZE]bool = undefined;
         var flip: [CHUNK_SIZE * CHUNK_SIZE]bool = undefined;
         q[d] = 1;
-        x[d] = -1;
+        pos[d] = -1;
 
         // Check each slice of the chunk one at a time
-        while (x[d] < CHUNK_SIZE) {
-            var n: usize = 0;
-            for (0..CHUNK_SIZE) |j| {
-                x[v] = @intCast(j);
-                for (0..CHUNK_SIZE) |k| {
-                    x[u] = @intCast(k);
+        while (pos[d] < CHUNK_SIZE) {
+            var mask_index: usize = 0;
+            for (0..CHUNK_SIZE) |i| {
+                pos[v] = @intCast(i);
+                for (0..CHUNK_SIZE) |j| {
+                    pos[u] = @intCast(j);
 
                     // q determines the direction (X, Y or Z) that we are searching
                     // self.is_block_at(x,y,z) takes global map positions and returns true if a block exists there
                     //TODO: add chunk position to both is_block_at call
-                    const blockCurrent: bool = if (0 <= x[d]) self.is_block_at(x[0], x[1], x[2]) else false;
-                    const blockCompare: bool = if (x[d] < CHUNK_SIZE - 1) self.is_block_at(x[0] + q[0], x[1] + q[1], x[2] + q[2]) else false;
+                    const blockCurrent: bool = if (0 <= pos[d]) self.is_block_at(pos[0], pos[1], pos[2]) else false;
+                    const blockCompare: bool = if (pos[d] < CHUNK_SIZE - 1) self.is_block_at(pos[0] + q[0], pos[1] + q[1], pos[2] + q[2]) else false;
 
                     // The mask is set to true if there is a visible face between two blocks,
                     //   i.e. both aren't empty and both aren't blocks
-                    mask[n] = blockCurrent != blockCompare;
+                    mask[mask_index] = blockCurrent != blockCompare;
                     //Flip is set to true if its normals should be flipped
-                    flip[n] = blockCompare;
-                    n += 1;
+                    flip[mask_index] = blockCompare;
+                    mask_index += 1;
                 }
             }
 
-            x[d] += 1;
-            n = 0;
+            pos[d] += 1;
+            mask_index = 0;
 
             // Generate a mesh from the mask using lexicographic ordering,
             //   by looping over each block in this slice of the chunk
             for (0..CHUNK_SIZE) |j| {
                 var i: usize = 0;
                 while (i < CHUNK_SIZE) {
-                    if (mask[n]) {
-                        var w: usize = 1;
-                        var h: usize = 1;
-                        // Compute the width of this quad and store it in w
-                        //   This is done by searching along the current axis until mask[n + w] is false
-                        while (i + w < CHUNK_SIZE and mask[n + w] and (flip[n] == flip[n + w])) : (w += 1) {}
+                    if (mask[mask_index]) {
+                        var width: usize = 1;
+                        var height: usize = 1;
+                        // Compute the width of this quad and store it in width
+                        //   This is done by searching along the current axis until mask[mask_index + width] is false
+                        while (i + width < CHUNK_SIZE and mask[mask_index + width] and (flip[mask_index] == flip[mask_index + width])) : (width += 1) {}
 
-                        // Compute the height of this quad and store it in h
-                        //   This is done by checking if every block next to this row (range 0 to w) is also part of the mask.
-                        //   For example, if w is 5 we currently have a quad of dimensions 1 x 5. To reduce triangle count,
+                        // Compute the height of this quad and store it in height
+                        //   This is done by checking if every block next to this row (range 0 to width) is also part of the mask.
+                        //   For example, if width is 5 we currently have a quad of dimensions 1 x 5. To reduce triangle count,
                         //   greedy meshing will attempt to expand this quad out to CHUNK_SIZE x 5, but will stop if it reaches a hole in the mask
                         var done = false;
-                        while (j + h < CHUNK_SIZE) : (h += 1) {
+                        while (j + height < CHUNK_SIZE) : (height += 1) {
                             // Check each block next to this quad
-                            for (0..w) |k| {
+                            for (0..width) |k| {
                                 // If there's a hole in the mask, exit
-                                if (!mask[n + k + h * CHUNK_SIZE] or flip[n] != flip[n + k + h * CHUNK_SIZE]) {
+                                if (!mask[mask_index + k + height * CHUNK_SIZE] or flip[mask_index] != flip[mask_index + k + height * CHUNK_SIZE]) {
                                     done = true;
                                     break;
                                 }
@@ -105,60 +105,52 @@ pub fn greedy_mesh(self: *Chunk) void {
                                 break;
                         }
 
-                        x[u] = @intCast(i);
-                        x[v] = @intCast(j);
+                        pos[u] = @intCast(i);
+                        pos[v] = @intCast(j);
 
                         // du and dv determine the size and orientation of this face
                         var du: [3]isize = .{ 0, 0, 0 };
-                        du[u] = @intCast(w);
+                        du[u] = @intCast(width);
 
                         var dv: [3]isize = .{ 0, 0, 0 };
-                        dv[v] = @intCast(h);
+                        dv[v] = @intCast(height);
 
                         //Create the vertices for the quad (to, you know, actually render the quad)
-                        const bottom_right = create_vertex(x[0] + du[0] + dv[0], x[1] + du[1] + dv[1], x[2] + du[2] + dv[2], 1.0, 1.0);
-                        const top_right = create_vertex(x[0] + du[0], x[1] + du[1], x[2] + du[2], 1.0, 0.0);
-                        const top_left = create_vertex(x[0], x[1], x[2], 0.0, 0.0);
-                        const bottom_left = create_vertex(x[0] + dv[0], x[1] + dv[1], x[2] + dv[2], 0.0, 1.0);
-                        //Now we need to append 6 vertices, as we use vertex pulling and need to form 2 triangles to form a quad.
-                        if (!flip[n]) { //Not flip means we don't want to be flip the normal!
-                            vertices[vertex_count + 0] = bottom_right;
-                            vertices[vertex_count + 1] = top_right;
-                            vertices[vertex_count + 2] = top_left;
+                        const bottom_right = create_vertex(pos[0] + du[0] + dv[0], pos[1] + du[1] + dv[1], pos[2] + du[2] + dv[2], 1.0, 1.0);
+                        const top_right = create_vertex(pos[0] + du[0], pos[1] + du[1], pos[2] + du[2], 1.0, 0.0);
+                        const top_left = create_vertex(pos[0], pos[1], pos[2], 0.0, 0.0);
+                        const bottom_left = create_vertex(pos[0] + dv[0], pos[1] + dv[1], pos[2] + dv[2], 0.0, 1.0);
 
-                            vertices[vertex_count + 3] = bottom_left;
-                            vertices[vertex_count + 4] = bottom_right;
-                            vertices[vertex_count + 5] = top_left;
-                        } else { //Here we want to flip the normal!
-                            vertices[vertex_count + 0] = top_left;
-                            vertices[vertex_count + 1] = top_right;
-                            vertices[vertex_count + 2] = bottom_right;
+                        const quad: [6]Vertex = if (!flip[mask_index]) .{
+                            bottom_right, top_right,    top_left,
+                            bottom_left,  bottom_right, top_left,
+                        } else .{
+                            top_left, top_right,    bottom_right,
+                            top_left, bottom_right, bottom_left,
+                        };
 
-                            vertices[vertex_count + 3] = top_left;
-                            vertices[vertex_count + 4] = bottom_right;
-                            vertices[vertex_count + 5] = bottom_left;
+                        for (quad) |vertex| {
+                            vertices_list.append(allocator, vertex) catch unreachable;
                         }
-                        //Done messing with vertices/quad, so increment!
-                        vertex_count += 6;
 
-                        for (0..h) |l| {
-                            for (0..w) |k| {
-                                mask[n + k + l * CHUNK_SIZE] = false;
+                        for (0..height) |l| {
+                            for (0..width) |k| {
+                                mask[mask_index + k + l * CHUNK_SIZE] = false;
                             }
                         }
 
-                        i += w;
-                        n += w;
+                        i += width;
+                        mask_index += width;
                     } else {
                         i += 1;
-                        n += 1;
+                        mask_index += 1;
                     }
                 }
             }
         }
     }
     self.storage_buffer = sg.makeBuffer(.{
-        .data = sg.asRange(&vertices),
+        .data = sg.asRange(vertices_list.items),
         .usage = .{ .storage_buffer = true },
     });
     self.ssbo_view = sg.makeView(.{
@@ -166,8 +158,8 @@ pub fn greedy_mesh(self: *Chunk) void {
             .buffer = self.storage_buffer,
         },
     });
-    self.vertex_count = vertex_count;
-    std.debug.print("v: {d}\n", .{vertex_count});
+    self.vertex_count = @intCast(vertices_list.items.len);
+    std.debug.print("v: {d}\n", .{self.vertex_count});
 }
 
 fn is_block_at(self: *Chunk, x: isize, y: isize, z: isize) bool {
